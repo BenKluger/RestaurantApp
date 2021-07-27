1075c31e211bc17b72569121e54c290342451c97
BenKluger <bennykluger@gmail.com>
2021-07-27 14:47:05 -0400

    Fixed issue I think

---
+++ GroupProjectMain.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class GroupProjectMain{
+
+
+    public static void main(String[] args){
+
+        String customerName = JOptionPane.showInputDialog("Please enter your name: ");
+
+        JOptionPane.showMessageDialog(null, "The customer's name is: " + customerName);
+
+
+
+    }
+
+
+    
+}
