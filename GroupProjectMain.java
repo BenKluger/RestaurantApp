3b0ef9ccfb782cd3586c03309e3505b461ccbdfe
BenKluger <bennykluger@gmail.com>
2021-07-27 14:52:17 -0400

    Set up the main. Please enter your names in the JavaDoc :x

--- GroupProjectMain.java
+++ GroupProjectMain.java
@@ -1,3 +1,14 @@
+
+/**
+ * @Description This is the main method of our group project. Inside of it you will be able to call whichever design pattern you want to test.
+ * 
+ * @Authors Ben Kluger, (Please add your names to this list, or DM me your name and I will add you)
+ * 
+ */
+
+
+
+
 import javax.swing.JOptionPane;
 
 public class GroupProjectMain{
@@ -5,11 +16,16 @@ public class GroupProjectMain{
 
     public static void main(String[] args){
 
-        String customerName = JOptionPane.showInputDialog("Please enter your name: ");
+       // String customerName = JOptionPane.showInputDialog("Please enter your name: ");
 
-        JOptionPane.showMessageDialog(null, "The customer's name is: " + customerName);
+       // JOptionPane.showMessageDialog(null, "The customer's name is: " + customerName);
 
+        String customerOrder = JOptionPane.showInputDialog("Please enter the design pattern that you want to test. The choices are: Factory, ");
 
+        if (customerOrder.equalsIgnoreCase("factory")){
+            System.out.println("You have chosen the factory pattern.");
+            //Include the code here to call the factory pattern
+        }
 
     }
 
