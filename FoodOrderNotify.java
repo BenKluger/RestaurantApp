b52ba13ebe9ac8d2763330aa00b89c8fd59cb2a1
KieunB102 <65143421+KieunB102@users.noreply.github.com>
2021-08-04 23:27:57 -0400

    Update FoodOrderNotify.java

--- FoodOrderNotify.java
+++ FoodOrderNotify.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
- 
+
+//notify the user of their food order status, that the order has been received by the restaurant 
 public class FoodOrderNotify implements FoodSubject {
-     
+    //List for food orders
     private List<Observer> foodOrders = new ArrayList<>();
  
     @Override
