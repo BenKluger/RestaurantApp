03ff198700cf1f18a67972f920bba0c9b6a1ea95
KieunB102 <65143421+KieunB102@users.noreply.github.com>
2021-08-08 02:08:51 -0400

    Update foodMessage.java

--- foodMessage.java
+++ foodMessage.java
@@ -1,8 +1,12 @@
+//foodMessage class will get the message content of the order
 public class foodMessage 
 {
+    //messageContent is set to final cause we don't want to change the value.
     final String messageContent;
      
     public foodMessage (String fOrder) {
+        //this keyword is used to be a reference to the current object.
+        //Here, we invoke the messageContent string and set it to fOrder
         this.messageContent = fOrder;
     }
  
