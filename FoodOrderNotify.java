709e4c9948987d62088ed31d976ec9f95eff93b4
KieunB102 <65143421+KieunB102@users.noreply.github.com>
2021-08-08 00:29:54 -0400

    Update FoodOrderNotify.java

--- FoodOrderNotify.java
+++ FoodOrderNotify.java
@@ -7,11 +7,13 @@ public class FoodOrderNotify implements FoodSubject {
     private List<Observer> foodOrders = new ArrayList<>();
  
     @Override
+    //status method with Observer order will add an order to the the list.
     public void status(Observer order) {
         foodOrders.add(order);
     }
  
    @Override
+    //notifyUpdate method will update each status of the order.
     public void notifyUpdate(foodMessage fOrder) {
         for(Observer order: foodOrders) {
             order.update(fOrder);
