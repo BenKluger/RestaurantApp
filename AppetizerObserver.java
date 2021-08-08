827024cff9e2d0a6c1f0bb2383c64c171945c1d6
KieunB102 <65143421+KieunB102@users.noreply.github.com>
2021-08-07 23:36:52 -0400

    Update AppetizerObserver.java

--- AppetizerObserver.java
+++ AppetizerObserver.java
@@ -1,6 +1,8 @@
+//AppetizerObserver is a public class that will implement Observer in order to print the update for the appetizer section.
 public class AppetizerObserver implements Observer 
 {
     @Override
+    // The update method will call the foodMessage fOrder to dislay the update.
     public void update(foodMessage fOrder) {
         System.out.println("AppetizerObserver: " + fOrder.getMessageContent());
     }
