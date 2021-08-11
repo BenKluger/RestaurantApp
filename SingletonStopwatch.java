bfbe0c12641d75e44961f8d0f973d0cc2f06cb7b
mmim14 <87958743+mmim14@users.noreply.github.com>
2021-08-11 17:33:56 -0400

    Update SingletonStopwatch.java

--- SingletonStopwatch.java
+++ SingletonStopwatch.java
@@ -1,41 +1,38 @@
-import java.sql.Time;
-
 /*
  * Momina Mim
- * CSCI 370 Project - Restaurant App 
- * SingletonStopwatch program will track the time from placement 
- * of the order until the order is done. Singleton will ensure there's only one 
- * stopwatch per order. No matter which class (who meaning cashier, chef, etc) 
- * they will access to only one time. 
+ * CSCI 370 Project - Restaurant App
+ * SingletonStopwatch program will track the time from placement
+ * of the order until the order is done. Singleton will ensure there's only one
+ * stopwatch per order. No matter which class (who meaning cashier, chef, etc)
+ * they will access to only one time.
  */
 
 public class SingletonStopwatch {
-	
-	private static SingletonStopwatch stopwatch;
-	private Time time;
-	private long startTime;
-	private long currentTime;
-	private long timeElapsed;
-	
-	private SingletonStopwatch() {
-		startTime = System.currentTimeMillis();
-	}
-	
-	//this will prevent classes from instantiating multiple instances
-	public static SingletonStopwatch Stopwatch() {
-		if(stopwatch == null) {
-			stopwatch = new SingletonStopwatch();
-		}
-		return stopwatch;
-	}
 
-	public String getTime() {
-		currentTime = System.currentTimeMillis();
-		
-		timeElapsed = currentTime - startTime;
-		
-		int minutes = (int) (timeElapsed/1000)/60;
-		int seconds = (int) (timeElapsed/1000)%60;
-		return ("Time Elapsed since order placed - " + minutes +":"+ seconds+ "\n"); 
-	}
+    private static SingletonStopwatch stopwatch;
+    private final long startTime;
+    private long currentTime;
+
+    private SingletonStopwatch() {
+        startTime = System.currentTimeMillis();
+        currentTime = 0;
+    }
+
+    //this will prevent classes from instantiating multiple instances
+    public static synchronized SingletonStopwatch Stopwatch() {
+        if(stopwatch == null) {
+            stopwatch = new SingletonStopwatch();
+        }
+        return stopwatch;
+    }
+
+    public String getTime() {
+        long timeElapsed;
+        currentTime = System.currentTimeMillis();
+        timeElapsed = currentTime - startTime;
+        int minutes = (int) (timeElapsed/1000)/60;
+        int seconds = (int) (timeElapsed/1000)%60;
+        return ("Time elapsed since the order was placed - " + minutes +":"+ seconds + " (m:s)");
+    }
+
 }
