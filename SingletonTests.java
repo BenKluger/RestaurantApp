51f418f81f506398b18a62f12a3ff875d6289b82
Eyal D <eyaldalal@hotmail.com>
2021-08-11 17:45:21 -0400

    Test update

--- SingletonTests.java
+++ SingletonTests.java
@@ -1,17 +1,19 @@
 //import static org.junit.Assert.*;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class SingletonTests {
 
     @Test
-    public void StopwatchTest() {
+    void StopwatchTest() {
         SingletonStopwatch sw1 = SingletonStopwatch.Stopwatch();
         SingletonStopwatch sw2 = SingletonStopwatch.Stopwatch();
         //it should return true since both of these objects should point
         //to the same memory location
-        Assert.assertTrue(sw1 == sw2);
-        Assert.assertSame(sw1, sw2);
+        assertTrue(sw1 == sw2);
+        assertSame(sw1, sw2);
         //^assertTrue and assertSame do the same test. It's just different ways of doing it
 
         //the test below fails; that's what we want
@@ -19,16 +21,16 @@ public class SingletonTests {
 
         CustomerSingleton customer1 = new CustomerSingleton();
         CustomerSingleton customer2 = new CustomerSingleton();
-        Assert.assertNotSame(customer1, customer2);
+        assertNotSame(customer1, customer2);
     }
 
     @Test
-    public void getTimeTest() {
+    void getTimeTest() {
         SingletonStopwatch sw1 = SingletonStopwatch.Stopwatch();
         ChefSingleton cs = new ChefSingleton();
         cs.prepareOrder();
         //this test checks the time is more than 0m:0s.
-        Assert.assertFalse(
+        assertFalse(
                 ("Time Elapsed since the order was placed - " + 0 + ":" + 0 + " (m:s)").contentEquals(sw1.getTime()));
     }
 
