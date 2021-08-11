4f2ecdfecb07a1a7c635d08370279cd0cfb6dcec
KieunB102 <65143421+KieunB102@users.noreply.github.com>
2021-08-11 17:07:40 -0400

    Update foodMessageTest.java

--- foodMessageTest.java
+++ foodMessageTest.java
@@ -1,16 +1,14 @@
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
     public class foodMessageTest {
      @Test
-        public foodMessage(){
+        public getMessageContent(){
          foodMessage foodmessage = new foodmessage();
 
          //The good input will lead to the message being received.
-         assertTrue(foodmessage.this.messageContent) instanceof foodMessage);
+         assertEquals(foodmessage.(this.messageContent), fOrder);
 
-        //bad input
-         assertFalse(foodmessage.this.messageContent) instanceof entreeObserver );
+         assertEquals(foodmessage.(this.messageContent), dessert );
      }
 }
