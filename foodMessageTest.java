02bfbd4a8493b30d73c4038e72efc6120151488d
KieunB102 <65143421+KieunB102@users.noreply.github.com>
2021-08-11 17:20:56 -0400

    Update foodMessageTest.java

--- foodMessageTest.java
+++ foodMessageTest.java
@@ -10,4 +10,12 @@ import static org.junit.jupiter.api.Assertions.*;
          assertEquals("Test", foodmessage.getMessageContent());
 
      }
+     //check the foodMessage() method to see what is being received.
+     @Test
+         void foodMessage(){
+         foodMessage foodmessage = new foodMessage("Test");
+
+         //The good input will lead to the message being received.
+         assertEquals("Test", foodmessage.getMessageContent());
+         }
 }
